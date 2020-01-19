c339beab38110dcf8beec037f9b980d64e5afeea
ropio09 <rociors@live.com>
2020-01-19 17:47:45 -0500

    testBase

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -9,5 +9,10 @@ import utils.ConfigReader;
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
+    public static WebDriver driver;
+    public static final String propertyPath= "src/main/resources/configurations/configurations.properties";
+    private static long implicitWait= Long.parseLong(ConfigReader.readProperty(propertyPath, "implicitWait"));
+    public static String url= ConfigReader.readProperty(propertyPath,"url");
+    public static String browser= ConfigReader.readProperty(propertyPath,"browser");
 
 }
