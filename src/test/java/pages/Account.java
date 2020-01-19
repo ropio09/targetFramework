79363502664478ee83d0cb80a3c0c5076e0eac36
ropio09 <rociors@live.com>
2020-01-19 17:52:15 -0500

    page account class

--- src/test/java/pages/Account.java
+++ src/test/java/pages/Account.java
@@ -4,10 +4,28 @@ import base.TestBase;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import utils.ConfigReader;
 import utils.SeleniumUtils;
 
 public class Account extends TestBase {
+    Account account;
+    HomePage homePage;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp(){
+        initializer();
+        homePage = new HomePage();
+        account = new Account();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown(){
+        driver.quit();
+    }
+
+
 
 
 
